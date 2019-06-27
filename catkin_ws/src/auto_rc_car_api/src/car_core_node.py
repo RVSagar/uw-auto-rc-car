1ce199f10bde4938ad4576386878b14fb623a23f
Wesley Fisher <wfisher@edu.uwaterloo.ca>
2019-06-27 11:32:06 -0400

    API and networking edits

---
+++ catkin_ws/src/auto_rc_car_api/src/car_core_node.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python2
+import rospy
+
+from sensor_msgs.msg import LaserScan
+from sensor_msgs.msg import Image
+
+from geometry_msgs.msg import Twist
+
+from auto_rc_car_api.srv import carLidar, carLidarResponse
+from auto_rc_car_api.srv import carCamera, carCameraResponse
+from auto_rc_car_api.srv import carControls, carControlsResponse
+from auto_rc_car_api.msg import carSteering
+
+
+class RawDataModule:
+    def __init__(self, rospy,
+                 msg_topic, msg_type,
+                 srv_topic, srv_type, srv_return_type):
+        self.msg_topic = msg_topic
+        self.msg_type = msg_type
+        self.srv_topic = srv_topic
+        self.srv_type = srv_type
+        self.srv_return_type = srv_return_type
+
+        self.data = self.msg_type()
+        self.sub = rospy.Subscriber(self.msg_topic, self.msg_type, self.data_cb)
+        self.srv = rospy.Service(self.srv_topic, self.srv_type, self.srv_cb)
+    
+    def data_cb(self, msg):
+        self.data = msg
+
+    def srv_cb(self, msg):
+        return self.srv_return_type(self.data)
+
+class AutoRCCar:
+
+    def __init__(self):
+
+        rospy.init_node('rc_car_core')
+
+        self.lidar = RawDataModule(rospy,
+                                   '/racecar/out/laser_scan', LaserScan,
+                                   'racecar_api/lidar', carLidar, carLidarResponse)
+        self.camera = RawDataModule(rospy,
+                                   '/camera/zed/rgb/image_rect_color', Image,
+                                   'racecar_api/camera', carCamera, carCameraResponse)
+
+        self.controls = RawDataModule(rospy,
+                                      '/racecar/api_internal/control', carSteering,
+                                      'racecar_api/last_control', carControls, carControlsResponse)
+
+        self.control_sub = rospy.Subscriber('/racecar/api_internal/control', carSteering, self.control_cb)
+        self.control_pup = rospy.Publisher('/racecar/in/cmd_vel', Twist, queue_size=3)
+
+        #self.laser_scan = None
+        #self.laser_scan_sub = rospy.Subscriber('/racecar/out/laser_scan', LaserScan, self.laser_scan_cb)
+        #self.laser_scan_service = rospy.Service('/racecar_api/lidar', Lidar, self.laser_scan_service_cb)
+
+    def control_cb(self, msg):
+        cmd = Twist()
+        cmd.linear.x = msg.speed
+        cmd.angular.z = msg.steer
+        self.control_pup.publish(cmd)
+
+
+    def run(self):
+        while not rospy.is_shutdown():
+            rospy.sleep(1)
+
+
+if __name__ == "__main__":
+    car = AutoRCCar()
+    car.run()
---
+++ catkin_ws/src/auto_rc_car_api/src/client.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python2
+import rospy
+import numpy as np
+
+from std_msgs.msg import Float64
+
+from auto_rc_car_api.msg import carSteering
+from auto_rc_car_api.srv import carLidar, carCamera, carControls
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+
+class AutoRCCarClient:
+
+    def __init__(self):
+
+        rospy.init_node('rc_car_client')
+        rospy.sleep(0.5)
+
+        lidar_topic = '/racecar_api/lidar'
+        rospy.wait_for_service(lidar_topic)
+        self.lidar_srv = rospy.ServiceProxy('/racecar_api/lidar', carLidar)
+
+        camera_topic = '/racecar_api/camera'
+        rospy.wait_for_service(camera_topic)
+        self.camera_srv = rospy.ServiceProxy(camera_topic, carCamera)
+
+        control_topic = '/racecar_api/last_control'
+        rospy.wait_for_service(control_topic)
+        self.control_srv = rospy.ServiceProxy(control_topic, carControls)
+
+        send_control_topic = '/racecar/api_internal/control'
+        self.control_pub = rospy.Publisher(send_control_topic, carSteering, queue_size=3)
+
+
+    def get_latest_lidar(self):
+        data, dt = self.timed_service(self.lidar_srv)
+        return data.lidar, dt
+
+    def get_latest_camera(self):
+        data, dt = self.timed_service(self.camera_srv)
+        return data.camera, dt
+
+    def get_latest_controls(self):
+        data, dt = self.timed_service(self.control_srv)
+        return data.controls, dt
+
+    def send_control(self, speed, steer):
+        msg = carSteering()
+        msg.speed = speed
+        msg.steer = steer
+        self.control_pub.publish(msg)
+
+    def camera_to_cv_image(self, data):
+        try:
+            cv_image = CvBridge().imgmsg_to_cv2(data, "bgr8")
+            return cv_image
+        except CvBridgeError as e:
+            print(e)
+
+    def timed_service(self, srv):
+        t1 = self.get_time()
+        ret = srv()
+        t2 = self.get_time()
+        return ret, (t2 - t1).to_sec()
+
+    def get_time(self):
+        return rospy.Time.now()
+
+
+if __name__ == "__main__":
+    car = AutoRCCarClient()
+    
+    rospy.sleep(1.0)
+
+    print("Starting")
+    dist = 0
+
+    while not rospy.is_shutdown():
+
+        if dist < 3:
+            car.send_control(-0.1,0)
+        elif dist > 5:
+            car.send_control(0.1, 0)
+
+        l, dt1 = car.get_latest_lidar()
+        tot = 0
+        num = 0
+        i = 0
+        for r in l.ranges:
+            angle = l.angle_min + i*l.angle_increment
+
+            if angle > -0.25 and angle < 0.25:
+                if not np.isinf(r) and not np.isnan(r) and not r == 0.0:
+                    tot += r
+                    num += 1
+            i += 1
+        
+        if num != 0:
+            dist = tot / num
+
+        img, dt2 = car.get_latest_camera()
+        img = car.camera_to_cv_image(img)
+
+        data = "Dist = %f     Data Transfer Time = %f" % (dist, dt1+dt2)
+        cv2.putText(img, data, (0,30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0))
+        cv2.imshow("Car Camera", img)
+        cv2.waitKey(1000)
