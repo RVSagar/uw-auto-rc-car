0ff1d3491d10c3c59f87b486741a800dbc09d275
Wesley Fisher <wfisher@edu.uwaterloo.ca>
2019-06-27 11:54:25 -0400

    Stuff

--- networking/set_as_client.sh
+++ networking/set_as_client.sh
@@ -3,7 +3,20 @@
 if [ -z "$1" ]
 then
       echo "Please call with the MASTER URI as an argument: ./set_as_client.sh xxx.yyy.zzz.www"
+      #return
 fi
 
-export ROS_MASTER_URI=http://$1:11311
-export ROS_IP=0.0.0.0
+#MASTER_IP=$1
+MASTER_IP=129.97.229.135
+export ROS_MASTER_URI=http://$MASTER_IP:11311
+#export ROS_IP=172.27.0.1
+
+LIST=(`hostname -I`)
+MY_IP=${LIST[0]}
+
+MY_IP=172.17.0.1
+#Y_IP=129.97.229.135
+
+echo "Setting ROS_IP=${MY_IP}"
+export ROS_IP=$MY_IP
+export ROS_HOSTNAME=$MY_IP
