41ad6e74bb9a6b54a67cda3734ff426e59752fce
Nirosh Manohara <niroshm14@gmail.com>
2019-12-13 16:26:09 -0500

    Made the car dodge the box

--- catkin_ws/src/auto_rc_car_demos/nodes/test_drive.py
+++ catkin_ws/src/auto_rc_car_demos/nodes/test_drive.py
@@ -7,14 +7,20 @@ import time
 
 from auto_rc_car_api.clients import CreateRCCar
 
-TIME_END = 18
+TIME_END = 12
+LEFT = 0.5
+RIGHT = -0.5
+STRAIGHT = 0
 
 def rad_to_deg(rad):
     deg = rad * (180 / math.pi)
     return deg
 
 def length_of_object(angle, sl1, sl2):
-    length = math.sqrt(math.pow(sl1, 2) + math.pow(sl2, 2) - (2*sl1*sl2*(math.cos(angle))))
+    sl1_sqrd = math.pow(sl1, 2)
+    sl2_sqrd = math.pow(sl2, 2)
+
+    length = math.sqrt(sl1_sqrd + sl2_sqrd - (2*sl1*sl2*math.cos(angle)))
     return length
 
 
@@ -23,32 +29,25 @@ if __name__ == "__main__":
    
     time_start = rospy.Time.now()   
 
-    lidar = car.get_latest_lidar()
+    # lidar = car.get_latest_lidar()
 
-    lidar_values = list(lidar[0].ranges)
-    
-    dist_arr = []
-    angle_arr = []
+    # lidar_values = list(lidar[0].ranges)
 
     increment = (math.pi / 720)
     ang_rad = -(math.pi / 720)
-    
-    # #print("lidar ranges: {}".format(lidar[0].ranges))
-    # print("Number of ranges: {}".format(len(lidar[0].ranges)))
-    # #print("The index is: {}".format(list_of_ranges))
 
-    for value in lidar_values:
-        ang_rad += increment
-        if type(value) == float and value != float("inf"):
-            dist_arr.append(value)
-            angle_arr.append(ang_rad)
+    # for value in lidar_values:
+    #     ang_rad += increment
+    #     if type(value) == float and value != float("inf"):
+    #         dist_arr.append(value)
+    #         angle_arr.append(ang_rad)
 
 
-    angle = rad_to_deg(angle_arr[-1] - angle_arr[0])
+    # angle = rad_to_deg(angle_arr[-1] - angle_arr[0])
 
-    mid = int(len(dist_arr) / 2)
+    mid = 360
 
-    length = length_of_object(angle, dist_arr[0], dist_arr[-1])
+    # length = length_of_object(angle, dist_arr[0], dist_arr[-1])
 
 
     # turn_right = car.send_control(10, 0.5)
@@ -61,18 +60,38 @@ if __name__ == "__main__":
         lidar = car.get_latest_lidar()
 
         list_of_ranges = list(lidar[0].ranges)
-        
-        car.send_control(8, 0)
 
-        mid = 360
+        dist_arr = []
+        angle_arr = []
+
+        for value in list_of_ranges:
+            ang_rad += increment
+            if type(value) == float and value != float("inf"):
+                dist_arr.append(value)
+                angle_arr.append(ang_rad)
+
+        angle = angle_arr[-1] - angle_arr[0]
+
+        length = length_of_object(angle, dist_arr[0], dist_arr[-1])
+
+        car.send_control(10, STRAIGHT)
+
+        if list_of_ranges[mid] < 1.5: 
+            car.send_control(10, 0.5)
+
 
         print("center dist: {}       length: {}".format(list_of_ranges[mid], length))
+
+        #print("First Dist: {}      Second Dist: {}      Angle: {}      Length: {}".format(dist_arr[0], dist_arr[-1], angle, length))
 
         # while(time.time() < time_right): 
         #     car.send_control(10, 0.5)
         
         # car.send_control(10, -0.5)
 
-        if (rospy.Time.now() - time_start).to_sec() > TIME_END or list_of_ranges[mid] < 0.5:
+        if list_of_ranges[mid] < 0.5:
+            car.send_control(0, 0)
+
+        if (rospy.Time.now() - time_start).to_sec() > TIME_END:
             car.send_control(0,0)
             break
