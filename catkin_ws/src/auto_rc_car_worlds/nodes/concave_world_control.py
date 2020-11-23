52beea135504ca07be735bcee245bb54e702ef38
Wesley Fisher <wfisher@edu.uwaterloo.ca>
2020-11-22 20:46:24 -0400

    Modify pedestrian code to prevent collisions

--- catkin_ws/src/auto_rc_car_worlds/nodes/concave_world_control.py
+++ catkin_ws/src/auto_rc_car_worlds/nodes/concave_world_control.py
@@ -6,29 +6,7 @@ import time
 
 from std_msgs.msg import Float64
 
-from world_objects import StopLight, Pedestrian
-
-
-class WorldController:
-    def __init__(self, stoplights, pedestrians):
-        self.t0 = rospy.Time.now().to_sec()
-        self.T = 0.1
-
-        self.stoplights = stoplights
-        self.pedestrians = pedestrians
-
-    def run(self):
-        while not rospy.is_shutdown():
-            self.control_loop()
-            rospy.sleep(self.T)
-
-    def control_loop(self):
-        t = rospy.Time.now().to_sec() - self.t0
-
-        for light in self.stoplights:
-            light.send(t)
-        for ped in self.pedestrians:
-            ped.send(t)
+from world_objects import StopLight, Pedestrian, WorldObserver, WorldController
 
 
 
@@ -36,18 +14,21 @@ if __name__ == "__main__":
     print("Running World Control")
     rospy.init_node("concave_world_control")
 
+    dt = 0.1
+    WO = WorldObserver()
+
     light0 = StopLight("sign0", 10, 10)
     light1 = StopLight("sign1", 8, 12)
     light2 = StopLight("sign2", 12, 8)
     light3 = StopLight("sign3", 7, 7)
     stoplights = [light0, light1, light2, light3]
 
-    ped0 = Pedestrian("pedestrian0", [0, 0, 2, 2, 0], [0, 10, 20, 30, 40])
-    ped1 = Pedestrian("pedestrian1", [0, 0, 1.75, 1.75, 0], [0, 7, 14, 21, 28])
-    ped2 = Pedestrian("pedestrian2", [0, 0, 2, 2, 0], [0, 5, 10, 15, 20])
-    ped3 = Pedestrian("pedestrian3", [0, 0, 1.75, 1.75, 0], [0, 7, 14, 21, 28])
+    ped0 = Pedestrian("pedestrian0", 2, 20, dt, WO)
+    ped1 = Pedestrian("pedestrian1", 1.75, 14, dt, WO)
+    ped2 = Pedestrian("pedestrian2", 2, 18, dt, WO)
+    ped3 = Pedestrian("pedestrian3", 1.75, 20, dt, WO)
     pedestrians = [ped0, ped1, ped2, ped3]
 
-    c = WorldController(stoplights, pedestrians)
+    c = WorldController(stoplights, pedestrians, dt)
     c.run()
 
