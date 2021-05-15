85722c41a58599b56d570d17c305315afef53491
Wesley Fisher <wesley_b_fisher@hotmail.com>
2021-05-14 21:08:25 -0300

    Create blank versions of worlds for easy first nn
    - Generate blank (no houses, stopsigns, pedestrians) verisons of worlds
    - Add arguments passable through launch files to control which is opened
    - Add argument to simple_lan_demo_sim

--- catkin_ws/src/auto_rc_car_worlds/scripts/gen_circle_world.py
+++ catkin_ws/src/auto_rc_car_worlds/scripts/gen_circle_world.py
@@ -9,35 +9,44 @@ from world_building import WorldBuilder
 
 if __name__ == "__main__":
     print("Running World Generator")
-    wb = WorldBuilder("circle_world.sdf", scale=0.125)
-    contents = wb.get_sources()
+    name = 'circle_world.sdf'
 
-    R = 65
-    road = []
-    for th in np.linspace(0, 6.29, 100):
-        x = math.cos(th)*R
-        y = math.sin(th)*R
-        y = y + R # Places (0,0) on the road
-        road.append((x, y, 0))
-    wb.add(contents, wb.generate_road_contents(road))
+    for blank in [True, False]:
+        if blank:
+            world_name = "blank_" + name
+        else:
+            world_name = name
 
-    
-    outer_houses = [1, 2, 3, 3, 2, 3, 2, 1, 2, 1 ,2, 3, 3, 2]
-    inner_houses = [2, 3, 2, 1, 1, 1, 2, 3, 1]
+        wb = WorldBuilder(world_name, scale=0.125)
+        contents = wb.get_sources()
 
-    for i in range(0, len(outer_houses)):
-        th = float(i) / len(outer_houses) * 6.28
-        x = math.cos(th)*(R+10)
-        y = math.sin(th)*(R+10) + R
-        model = outer_houses[i]
-        wb.add(contents, wb.generate_house_contents(model, x, y, th-1.5707))
+        R = 65
+        road = []
+        for th in np.linspace(0, 6.29, 100):
+            x = math.cos(th)*R
+            y = math.sin(th)*R
+            y = y + R # Places (0,0) on the road
+            road.append((x, y, 0))
+        wb.add(contents, wb.generate_road_contents(road))
 
-    for i in range(0, len(inner_houses)):
-        th = float(i) / len(inner_houses) * 6.28
-        x = math.cos(th)*(R-10)
-        y = math.sin(th)*(R-10) + R
-        model = inner_houses[i]
-        wb.add(contents, wb.generate_house_contents(model, x, y, th+1.5707))
+        
+        if not blank:
+            outer_houses = [1, 2, 3, 3, 2, 3, 2, 1, 2, 1 ,2, 3, 3, 2]
+            inner_houses = [2, 3, 2, 1, 1, 1, 2, 3, 1]
+
+            for i in range(0, len(outer_houses)):
+                th = float(i) / len(outer_houses) * 6.28
+                x = math.cos(th)*(R+10)
+                y = math.sin(th)*(R+10) + R
+                model = outer_houses[i]
+                wb.add(contents, wb.generate_house_contents(model, x, y, th-1.5707))
+
+            for i in range(0, len(inner_houses)):
+                th = float(i) / len(inner_houses) * 6.28
+                x = math.cos(th)*(R-10)
+                y = math.sin(th)*(R-10) + R
+                model = inner_houses[i]
+                wb.add(contents, wb.generate_house_contents(model, x, y, th+1.5707))
 
     wb.write_dest(contents)
 
--- catkin_ws/src/auto_rc_car_worlds/scripts/gen_city_world.py
+++ catkin_ws/src/auto_rc_car_worlds/scripts/gen_city_world.py
@@ -9,47 +9,56 @@ from world_building import WorldBuilder
 
 if __name__ == "__main__":
     print("Running World Generator")
-    wb = WorldBuilder("small_city_world.sdf", scale=0.125)
-    contents = wb.get_sources()
+    name = 'small_city_world.sdf'
 
-    wb.add(contents, wb.generate_road_contents([(wb.brs, 0, 0),
-                                                (40-wb.brs, 0, 0)]))
+    for blank in [True, False]:
+        if blank:
+            world_name = "blank_" + name
+        else:
+            world_name = name
 
-    wb.add(contents, wb.generate_road_contents([(0, wb.brs, 0),
-                                                (0, 40-wb.brs, 0)]))
+        wb = WorldBuilder(world_name, scale=0.125)
+        contents = wb.get_sources()
 
-    wb.add(contents, wb.generate_road_contents([(40, wb.brs, 0),
-                                                (40, 40-wb.brs, 0)]))
+        wb.add(contents, wb.generate_road_contents([(wb.brs, 0, 0),
+                                                    (40-wb.brs, 0, 0)]))
 
-    wb.add(contents, wb.generate_road_contents([(wb.brs, 40, 0),
-                                                (40-wb.brs,   40, 0)]))
+        wb.add(contents, wb.generate_road_contents([(0, wb.brs, 0),
+                                                    (0, 40-wb.brs, 0)]))
 
-    wb.add(contents, wb.generate_corner(0, 0, 0))
-    wb.add(contents, wb.generate_corner(0, 40, 1.5707))
-    wb.add(contents, wb.generate_3way(40, 0, 0))
-    wb.add(contents, wb.generate_3way(40, 40, 3.1415))
+        wb.add(contents, wb.generate_road_contents([(40, wb.brs, 0),
+                                                    (40, 40-wb.brs, 0)]))
 
-    # Create Curved Road
-    angles = np.linspace(0, 3.1415, 10)
-    points = []
-    x0 = 40 + wb.brs
-    y0 = 20
-    points.append((x0, y0-20, 0))
-    for a in angles:
-        x = x0 + 20*math.sin(a)+1
-        y = y0 - 20*math.cos(a)
-        points.append((x, y, 0))
-    points.append((x0, y0+20, 0))
-    wb.add(contents, wb.generate_road_contents(points))
-    
-    wb.add(contents, wb.generate_house_contents(1, 10, 10, 0))
-    wb.add(contents, wb.generate_house_contents(1, 15, 30, 3.1415))
+        wb.add(contents, wb.generate_road_contents([(wb.brs, 40, 0),
+                                                    (40-wb.brs,   40, 0)]))
 
-    wb.add(contents, wb.generate_house_contents(2, 30, 25, 1.5707)) 
+        wb.add(contents, wb.generate_corner(0, 0, 0))
+        wb.add(contents, wb.generate_corner(0, 40, 1.5707))
+        wb.add(contents, wb.generate_3way(40, 0, 0))
+        wb.add(contents, wb.generate_3way(40, 40, 3.1415))
 
-    wb.add(contents, wb.generate_house_contents(3, 50, 20, 1.5707))
+        # Create Curved Road
+        angles = np.linspace(0, 3.1415, 10)
+        points = []
+        x0 = 40 + wb.brs
+        y0 = 20
+        points.append((x0, y0-20, 0))
+        for a in angles:
+            x = x0 + 20*math.sin(a)+1
+            y = y0 - 20*math.cos(a)
+            points.append((x, y, 0))
+        points.append((x0, y0+20, 0))
+        wb.add(contents, wb.generate_road_contents(points))
+        
 
-                                    
+        if not blank:
+            wb.add(contents, wb.generate_house_contents(1, 10, 10, 0))
+            wb.add(contents, wb.generate_house_contents(1, 15, 30, 3.1415))
+
+            wb.add(contents, wb.generate_house_contents(2, 30, 25, 1.5707)) 
+
+            wb.add(contents, wb.generate_house_contents(3, 50, 20, 1.5707))
+
 
     wb.write_dest(contents)
 
